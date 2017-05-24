67ecb19d76ccfc7beb18a56028ce0c54fcd73dac
Alexandra Berke <berke.alexandra@gmail.com>
2017-05-24 15:23:15 -0400

    moved more of the styling to CSS and cleaned up some JS let vs var's

--- src/app/common/directives/canvas-centered-drawing.js
+++ src/app/common/directives/canvas-centered-drawing.js
@@ -1,37 +1,38 @@
 function canvasCenteredDrawingDirective($window) {
 	return {
-		restrict: 'EAC', //E = element, A = attribute, C = class, M = comment
-		link: function ($scope, element, attrs) {
+		restrict: 'EA', //E = element, A = attribute, C = class, M = comment
+		link: function (scope, element, attrs) {
 			//DOM manipulation
 
 			// get the element to draw canvas paper on and make it sq
-			var elt = element[0];
+			let elt = element[0];
 			elt.style.height = (String(elt.clientWidth) + "px");
 			elt.style.padding = 0; // remove padding used to make square with CSS
 
-			var paper = new Raphael(elt);
-			// add the canvas class in case not already there
-			elt.className += " canvas";
+			let paper = new Raphael(elt);
+			// add canvas-centered-drawing class + the canvas class in case not already there
+			elt.className += " canvas canvas-centered-drawing";
 
 			// get the drawFunction
-			var drawFunction = attrs.drawFunction ? $window[attrs.drawFunction] : 'drawCircle';
+			let drawFunction = attrs.drawFunction ? $window[attrs.drawFunction] : 'drawCircle';
 
 			// safely get the options -- yeah not that safe
-			var functionOptions = {};
-			var options = {};
+			let functionOptions = {};
+			let options = {};
 			try {
 				functionOptions = JSON.parse(attrs.functionOptions || "{}");
 				options = JSON.parse(attrs.options || "{}");
 			} catch(e) {}
 
-			let drawing = drawInCanvasCenter(paper, drawFunction, functionOptions, options);
+			scope.drawing = drawInCanvasCenter(paper, drawFunction, functionOptions, options);
 			
 			// if this is a part of a text-content-graphic, style for it
+			// CSS styles do not work here :(
 			let eltParent = element.parent();
 			let eltGrandparent = element.parent().parent();
 			if (eltParent.hasClass("text-content-graphic") || eltGrandparent.hasClass("text-content-graphic")) {
 
-				drawing.pathSet.attr({
+				scope.drawing.pathSet.attr({
 				    'stroke-width': options['stroke-width'] || 2,
 				    // default linecap is 'butt' which doesn't look right with thick strokes
 				    // Docs: https://www.w3.org/TR/SVG/painting.html#StrokeLinecapProperty
