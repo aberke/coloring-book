45f3ca6e0362fe3f2bf44c6806af1a474d5b8538
Alexandra Berke <berke.alexandra@gmail.com>
2018-02-02 09:15:59 -0500

    added facebook and twitter share button on info page
    - there is a tweet directive

---
+++ src/app/common/directives/tweet.js
@@ -0,0 +1,25 @@
+/**
+Directive for tweet button.
+
+Use:
+------
+<div tweet class="share-btn"></div>
+***/
+function tweetDirective($timeout) {
+    return {
+        restrict: "EA",
+        link: function(scope, element, attr) {
+            $timeout(function() {
+                twttr.widgets.createShareButton(
+                    "http://www.coloring-book.co",
+                    element[0],
+                    function(el) {}, {
+                        count: "none",
+                        hashtags: "colorByMath",
+                        text: "Math is about more than just numbers. In this 'book' the story of math is visual, told in shapes and patterns. This is a coloring book about group theory."
+                    }
+                );
+            });
+        }
+    };
+}
